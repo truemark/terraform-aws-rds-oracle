17f2b4ec46f2d18acddfb1b18b055dc805152969
Kyle Stephenson <kstephenson@youngliving.com>
2024-03-11 17:09:52 -0600

    latest versions

--- outputs.tf
+++ outputs.tf
@@ -23,11 +23,6 @@ output "db_instance_hosted_zone_id" {
   value       = module.db.db_instance_hosted_zone_id
 }
 
-output "db_instance_id" {
-  description = "The RDS instance ID"
-  value       = module.db.db_instance_id
-}
-
 output "db_instance_resource_id" {
   description = "The RDS Resource ID of this instance"
   value       = module.db.db_instance_resource_id
@@ -40,18 +35,12 @@ output "db_instance_status" {
 
 output "db_instance_name" {
   description = "The database name"
-  value       = module.db.db_instance_name
+  value       = lower(module.db.db_instance_name)
 }
 
 output "db_instance_username" {
   description = "The master username for the database"
   value       = module.db.db_instance_username
-  sensitive   = true
-}
-
-output "db_instance_password" {
-  description = "The database password (this password may be old, because Terraform doesn't track it after initial creation)"
-  value       = module.db.db_instance_password
   sensitive   = true
 }
 
